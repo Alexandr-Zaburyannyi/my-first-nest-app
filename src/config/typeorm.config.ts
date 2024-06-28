f3f7810b2f3dd7c026acaa166cb3b55569668ef7
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2024-06-28 11:31:57 +0200

    feat:configuring project for production environment

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,13 +7,27 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    if (process.env.NODE_ENV !== 'production') {
+      return {
+        type: 'sqlite',
+        synchronize: process.env.NODE_ENV === 'test' ? true : false,
+        database: this.configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
+        keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
+      };
+    }
     return {
-      type: 'sqlite',
-      synchronize: process.env.NODE_ENV === 'test' ? true : false,
+      type: 'postgres',
+      synchronize: false,
       database: this.configService.get<string>('DB_NAME'),
       autoLoadEntities: true,
-      migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-      keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
+      migrationsRun: true,
+      keepConnectionAlive: true,
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     };
   }
 }
