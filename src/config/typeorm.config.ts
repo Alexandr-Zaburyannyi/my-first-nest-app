2c410b27af49f501850fb347af98a36e8f949c1b
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2024-06-29 10:59:10 +0200

    feat:minor fix was provided

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,7 +9,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
     if (process.env.NODE_ENV !== 'production') {
       return {
-        type: 'sqlite',
+        type: process.env.NODE_ENV === 'production' ? 'postgres' : 'sqlite',
         synchronize: process.env.NODE_ENV === 'test' ? true : false,
         database: this.configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
